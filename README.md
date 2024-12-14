e0eb2ac685e454be489e7ab0bf4cc49f804529e3
mancuoj <mancuoj@gmail.com>
2024-12-14 17:19:50 +0800

    chore: update readme

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ pnpm db:push
 pnpm db:seed
 pnpm dev
 ```
+
+To migrate from local SQLite to Turso, take a look at [this commit](https://github.com/mancuoj-collective/next-learn-dashboard/commit/9251e7dda11a746bfb224a87526b89fde45121db).
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: 'Acme Dashboard',
-  description: 'Rewrite Next.js Learn Course with Drizzle ORM, SQLite and Shadcn/UI',
+  description: 'Next.js Learn Course Reimplemented with Drizzle ORM, SQLite and Shadcn/UI',
   url: 'https://next-learn-dashboard.mancuoj.me',
   umamiUrl: 'https://a.mancuoj.me/script.js',
   umamiId: 'df24efa9-53b2-486c-8bff-03a00872cb1b',
