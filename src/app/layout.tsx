45532878dc22182181e8c17b557a2aa7f24eed2d
mancuoj <mancuoj@gmail.com>
2024-12-23 18:58:33 +0800

    chore: update umami

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,7 @@ import '@/globals.css'
 
 import type { Metadata } from 'next'
 import { Inter, Lusitana } from 'next/font/google'
-import Script from 'next/script'
 
-import { env } from '@/config/env'
 import { siteConfig } from '@/config/site'
 import { cn } from '@/lib/utils'
 
@@ -29,9 +27,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn('font-sans antialiased', inter.variable, lusitana.variable)}>
-        {env.NODE_ENV === 'production' && (
-          <Script defer src={siteConfig.umamiUrl} data-website-id={siteConfig.umamiId} />
-        )}
         <AppProvider>{children}</AppProvider>
       </body>
     </html>
--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,15 +1,20 @@
 'use client'
 
 import { Provider } from 'jotai'
+import Script from 'next/script'
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
 
 import { ThemeScript, ThemeStyle } from '@/components/theme'
 import { Toaster } from '@/components/ui/sonner'
+import { siteConfig } from '@/config/site'
 
 export function AppProvider({ children }: React.PropsWithChildren) {
   return (
     <NuqsAdapter>
       <Provider>
+        {process.env.NODE_ENV === 'production' && (
+          <Script defer src={siteConfig.umamiUrl} data-website-id={siteConfig.umamiId} />
+        )}
         <ThemeScript />
         <ThemeStyle />
         {children}
